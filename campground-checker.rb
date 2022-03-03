c97a1c2330f6d835b6cabb9ad50ff20655c237c6
jenzhng <zhongjenn@gmail.com>
2022-03-03 06:40:54 -0500

    Update campground-checker.rb

--- campground-checker.rb
+++ campground-checker.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/ruby -w
-require 'nokogiri'
 require 'open-uri'
 require 'pry'
 require 'optparse'
@@ -9,62 +8,328 @@ require 'net/http'
 require 'rest-client'
 
 
-BASE_URL = "https://www.recreation.gov"
-#AVAILABILITY_ENDPOINT = "/api/camps/availability/campground/"
-MAIN_PAGE_ENDPOINT = "/api/camps/campgrounds/"
-TEST_URL = "https://www.recreation.gov/api/camps/availability/campground/"
-
-
 class Scraper
 	
 
-	def get_info
+	def main
 		
-		#get user input with options for park id, start date, and end date
-		options = {}
+		#get user input with options for park id, start date, and end date, campsite type, campsite id
+		
+		options = {
+		:parks => [], 
+		:campsite_ids => []
+		}
 		OptionParser.new do |opt|
 		opt.on('--start_date STARTDATE', Time) { |o| options[:start_date] = o.strftime("%F")}
 		opt.on('--end_date STARTDATE', Time) { |o| options[:end_date] = o.strftime("%F")}
-		opt.on('--parks PARKS') { |o| options[:parks] = o }
+		opt.on('-p', '--parks PARKS1,PARKS2,...', Array, "Parks")	do |f|
+				options[:parks] += f 
+			end
+		opt.on('--campsite_type TYPE') { |o| options[:campsite_type] = o }
+		opt.on('-p', '--campsite_ids IDS1,IDS2,...', Array, "Ids")	do |f|
+				options[:campsite_ids] += f 
+			end
+		opt.on('--nights NIGHTS') { |o| options[:nights] = o }
 		end.parse!
 		
 		puts options
 		
-		#json get request from reservation.gov api
-		temp_url = TEST_URL << options[:parks] <<
-		"/month?start_date=" << options[:start_date] << "T00%3A00%3A00.000Z"
-		html = URI.parse(temp_url)
-		response = Net::HTTP.get_response(html)
-		response.body
 		
-		camps = JSON.parse(response.body)
 		
 
-		get_name_of_park(options[:parks])
+		json_output=false
+	
+		info_by_park_id = {}
+		for park_id in options[:parks]
+			info_by_park_id[park_id] = check_park(
+            		park_id,
+           		 options[:start_date],
+            		options[:end_date], 
+			options[:campsite_type], 
+			options[:campsite_ids], 
+			options[:nights]
+			
+        	)
+		end
+	
+		if json_output
+			output, has_availabilities = generate_json_output(info_by_park_id)
+		else 
+			output, has_availabilities = generate_human_output(
+            		info_by_park_id,
+            		options[:start_date],
+            		options[:end_date]
+        	)
+		end
+		print output
+		return has_availabilities
 
-		
-		
-		
-		binding.pry
 		
 	end
 	
-	def get_name_of_park(park_id)
-		
-		#parse json to get park name
-		url = BASE_URL << MAIN_PAGE_ENDPOINT << park_id
-		page = URI.parse(url)
-		resp = Net::HTTP.get_response(page)
-		park_name = JSON.parse(resp.body)
 
-		puts park_name["campground"]["facility_name"]
+	def get_park_info(
+    park_id, start_date, end_date, campsite_type="", campsite_ids=[]
+)
+		#get the first of each month in the chosen range
+		start_of_month = Date.new(Date.parse(start_date).year, Date.parse(start_date).mon, 1)
+		
+		rrule = RRule::Rule.new('FREQ=MONTHLY',dtstart: DateTime.parse(start_of_month.to_s))
+		 
+		
+		months = rrule.between(DateTime.parse(start_of_month.to_s), DateTime.parse(end_date)).to_a
+		
+		#get data from each month
+		api_data = []
+		for month_date in months do
+			api_data.push(RecreationClient.get_availability(park_id, month_date))
+		end
+		
+		#collapse data into described output format
+		#filter by campsite_type if necessary
+		data = {}
+		
+		for month_data in api_data
+			
+			
+			for campsite_id, campsite_data in month_data["campsites"]
+				available = []
+				
+				data.store(campsite_id,[])
+				for date, availability_value in campsite_data["availabilities"]
+				
+					if availability_value != "Available" then
+						next
+					
+					elsif (campsite_type and campsite_type != campsite_data["campsite_type"] 
+					) then
+						next
+					
+					elsif (campsite_ids ==true) && (campsite_ids.include?(campsite_data["campsite_id"].to_i) == false) then
+                 
+						next
+					end
+					available.append(date)
+				end
+				if available
+					data[campsite_id]= available
+				end
+				
+			end
+		end
+		
+		
+			
+		return data
+	end
+				
+	def check_park(park_id, start_date, end_date, campsite_type, campsite_ids=[], nights=nil
+	)
+		park_info = get_park_info(park_id, start_date, end_date, campsite_type, campsite_ids
+		)
+		#print park_info
+		# print (
+			# "Information for park #{park_id}: "
+			# )
+		park_name = RecreationClient.get_park_name(park_id)
+
+		current, maximum, availabilities_filtered = 
+		get_availability(park_info, start_date, end_date, nights=nights
+		 )
+		 
+		return current, maximum, availabilities_filtered, park_name
+		
+		
+	end
+	
+	
+	def get_availability(park_info, start_date, end_date, nights=nil
+	)
+		nights = nights.to_i
+		num_available = 0
+		maximum = park_info.length
+		start = DateTime.parse(start_date)
+		end_d = DateTime.parse(end_date)
+		
+		num_days = (end_d - start).to_i
+		
+		dates = []
+		
+		for i in 1..(num_days) do 
+			
+			difference = end_d - i
+			s_date = DateTime.parse(difference.to_s).strftime( "%Y-%m-%dT00:00:00Z")
+			 dates.append(s_date)
+		end
+		
+		if (nights.between?(1, num_days) == false) 
+			nights = num_days
+			print ("Setting number of nights to #{nights}.")
+		end
+		
+		
+		
+		available_dates_by_campsite_id = Hash.new{|h, k| h[k] = []}
+		for site, availabilities in park_info
+			desired_available = []
+			for date in availabilities
+				
+				if (dates.include?(date) == false)
+					next
+				
+				end
+					desired_available.append(date)
+				
+			end
+			unless desired_available
+				next
+			end	
+			
+			
+			appropriate_consecutive_ranges = consecutive_nights(
+            desired_available, nights
+			)
+			
+			
+			if  appropriate_consecutive_ranges != []
+				num_available += 1
+				#print ("Available site #{num_available} : #{site}")
+			end
+			
+			for r in appropriate_consecutive_ranges
+				start, end_r = r
+				
+				
+				available_dates_by_campsite_id[site.to_i].append("start": start, "end": end_r)
+				
+				
+			end
+			
+			
+		end
+
+		return num_available, maximum, available_dates_by_campsite_id
+
 	end
 
+	def consecutive_nights(available, nights)
+				start_d = DateTime.new(1,1,1)
+			
+		ordinal_dates = []
+		for dstr in available 
+				
+				o_time = (DateTime.parse(dstr) - start_d - 1).to_i
+				
+				ordinal_dates.append(o_time)
+				
+			
+		end
+		 
+		
+		enum = ordinal_dates.slice_when { |x,y| y > x+1 }
+		consective_ranges = enum.to_a
+		
+		long_enough_consecutive_ranges = []
+		#print consective_ranges
+		for r in consective_ranges
+       		#skip ranges less than length of stay
+			if r.length < nights
+				next
+			end
+			for start_index in 0..r.length - nights
+					
+				# start_nice = start_d.next_day(r[start_index]+1)
+				start_nice = DateTime.parse(start_d.next_day(r[start_index]+1).to_s).strftime( "%Y-%m-%d")
+				
+				end_nice = DateTime.parse(start_d.next_day(r[start_index + nights - 1] + 2).to_s).strftime( "%Y-%m-%d")
+				
+				
+				long_enough_consecutive_ranges.append([start_nice, end_nice])
+				
+			end
+			
+			
+		end
+		
+		
+		return long_enough_consecutive_ranges
+
+	end
+
+	def generate_human_output(
+    info_by_park_id, start_date, end_date, gen_campsite_info=false
+)
+	out = []
+    	has_availabilities = false
+	
+		for park_id, info in info_by_park_id
+			current, maximum, available_dates_by_site_id, park_name = info
+			if current > 0
+				print ("SUCCESS ")
+				has_availabilities = true
+		
+			else
+				print ("FAILURE ")
+			end
+			out.append(
+            "#{park_name} (#{park_id}): #{current} site(s) available out of #{maximum} site(s)"  
+            
+            )
+			if gen_campsite_info and available_dates_by_site_id
+				for site_id, dates in available_dates_by_site_id
+                out.append(
+                    "  * Site #{site_id} is available on the following dates:"
+                )
+				
+					for date in dates
+					start=date["start"]
+					end_s=date["end"]
+                    out.append(
+                        "    * #{start} -> #{end_s}"
+                            
+                        
+                    )
+					end
+				end
+			end
+		
+	
+		end
+		if has_availabilities
+			start=DateTime.parse(start_date.to_s).strftime( "%Y-%m-%d")
+			end_p=DateTime.parse(end_date.to_s).strftime( "%Y-%m-%d")
+			out.insert(
+            0,
+            "there are campsites available from #{start} to #{end_p}!!!"
+            
+			)
+		else
+        out.insert(0, "There are no campsites available :(")
+		end
+	
+		return (out).join("\n"), has_availabilities
+
+	end
+
+	def generate_json_output(info_by_park_id)
+		availabilities_by_park_id = {}
+		has_availabilities = false
+		for park_id, info in info_by_park_id
+        current, _, available_dates_by_site_id, _ = info
+			if current
+				has_availabilities = true
+				availabilities_by_park_id[park_id] = available_dates_by_site_id
+			end
+		end
+		
+		return json.dump(availabilities_by_park_id), has_availabilities
+	end
+	
 	
 end
 
 	
 scrape = Scraper.new
-scrape.get_info
+scrape.main
 
 
